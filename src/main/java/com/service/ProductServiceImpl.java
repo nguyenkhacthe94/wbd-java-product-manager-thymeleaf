4da094a9b7cb16423f8ca64303438ffcc664ec52
Himedere <nguyenkhacthe94@gmail.com>
2018-05-30 16:43:30 +0700

    product manager

---
+++ src/main/java/com/service/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package com.service;
+
+import com.model.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductServiceImpl implements ProductService {
+    private static Map<Integer, Product> products;
+
+    static {
+        products = new HashMap<>();
+        products.put(1, new Product(1, "Dream", "Honda", 1000));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    @Override
+    public void save(Product product) {
+        products.put(product.getId(), product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return products.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        products.put(id, product);
+    }
+
+    @Override
+    public void remove(int id) {
+        products.remove(id);
+    }
+}
